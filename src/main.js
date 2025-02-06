da59ab2785fa2e154af0ba6e096ce73d105317a0
logan-codes <loganandamuthu2005@gmail.com>
2025-02-06 22:47:30 +0530

    Refactor: cleaned up the code

--- src/main.js
+++ src/main.js
@@ -8,45 +8,49 @@ let loop;
 let time;
 let timerID;
 
+function ToggleMenu(){
+    SFXaudio.pause();
+    SFXaudio.play();
+    ToggleDis("DropdownItems");
+    if (document.getElementsByClassName("DropdownItems")[0].style.display=="flex"){
+        document.getElementById("MenuIcon").src="media/images/close.png";
+    }   
+    else{
+        document.getElementById("MenuIcon").src="media/images/menu.png";
+    }
 
-function Start(){
-    toggleDis("startbtn");
-    toggleDis("modeSelec");
-    SFXaudio.play();
-    document.getElementsByClassName("welcometxt")[0].innerHTML="Select a mode to start the game";
+    if (timerID){
+        clearInterval(timerID);
+        timerID=null;
+    }
+    else{
+        StartTimer(time,"Countdown");
+    }
+
+    if (document.getElementsByClassName("Settings-page")[0].style.display=="flex"){
+        ToggleDis("Settings-page");
+    }
+    if (document.getElementsByClassName("Htp-page")[0].style.display=="flex"){
+        ToggleDis("Htp-page");
+    }
 }
-function Casual(){
-    document.getElementsByClassName("welcometxt")[0].innerHTML="Welcome to the Tic Tac Toe Game";
-    BGaudio.pause();
-    CBGaudio.play();
+
+function Resume(){
     SFXaudio.play();
-    mode="casual";
-    toggleDis("stage1");
-    toggleDis("stage2");
-    document.body.style.backgroundColor= "rgb(94, 4, 4)";
-    toggleDis("Resume");
-    toggleDis("MainMenu")
-    toggleDis("countdown");
-}
-function QuickDraw(){
-    document.getElementsByClassName("welcometxt")[0].innerHTML="Welcome to the Tic Tac Toe Game";
-    BGaudio.pause();
-    QBGaudio.play();
-    SFXaudio.play();
-    mode="quickDraw";
-    toggleDis("stage1");
-    toggleDis("stage2");
-    document.body.style.backgroundColor= "rgb(94, 4, 4)";
-    toggleDis("Resume");
-    toggleDis("MainMenu");
-    timer(10,"countdown");
-    loop =setInterval(()=>{
-        if(document.getElementsByClassName("countdown")[0].innerHTML=="Time's up"){
-            let ply= turn%2==0?"player2":"player1";
-            EndGame(ply+"has won. due to Time out");
-            clearInterval(loop);
-        }
-    },1000);
+    if (document.getElementsByClassName("Settings-page")[0].style.display=="flex"){
+        ToggleDis("Settings-page");
+    }
+    if (document.getElementsByClassName("Htp-page")[0].style.display=="flex"){
+        ToggleDis("Htp-page");
+    }
+    ToggleDis("DropdownItems");
+    if (document.getElementById("MenuIcon").src=="media/images/close.png"){
+        document.getElementById("MenuIcon").src="media/images/menu.png";
+    }
+    else{
+        document.getElementById("MenuIcon").src="media/images/close.png";
+    }
+    StartTimer(time,"Countdown");
 }
 
 function MainMenu(){
@@ -54,74 +58,186 @@ function MainMenu(){
     BGaudio.play();
     CBGaudio.pause();
     QBGaudio.pause();
-    reset();
-    toggleDis("stage1");
-    toggleDis("stage2");
-    toggleDis("stage3");
+    Reset();
+    ToggleDis("Stage1");
+    ToggleDis("Stage2");
+    ToggleDis("Stage3");
     p1s=0;
     p2s=0;
-    updateScore();
+    UpdateScore();
     document.body.style.backgroundColor= "rgb(255, 255, 255)";
-    toggleMenu();
-    toggleDis("Resume");
-    toggleDis("MainMenu");
-    toggleDis("startbtn");
-    toggleDis("modeSelec");
-    stopTimer();
+    ToggleMenu();
+    ToggleDis("Resume");
+    ToggleDis("MainMenu");
+    ToggleDis("StartBtn");
+    ToggleDis("ModeSelec");
+    StopTimer();
     clearInterval(loop);
     if(mode=="casual"){
-        toggleDis("countdown");
+        ToggleDis("Countdown");
     }
 }
+
 function Settings(){
     SFXaudio.play();
     if (document.getElementsByClassName("Htp-page")[0].style.display=="flex"){
-        toggleDis("Htp-page");
+        ToggleDis("Htp-page");
     }
-    toggleDis("Settings-page");
+    ToggleDis("Settings-page");
 }
+
 function Htp(){
     SFXaudio.play();
     if (document.getElementsByClassName("Settings-page")[0].style.display=="flex"){
-        toggleDis("Settings-page");
+        ToggleDis("Settings-page");
     }
-    toggleDis("Htp-page");
+    ToggleDis("Htp-page");
 }
+
+function BGvolume(){
+    var bgv = document.getElementById("BGvolume").value;
+    BGaudio.volume = bgv/100;
+    CBGaudio.volume = bgv/100;
+    QBGaudio.volume = bgv/100;
+}
+
+function SFXvolume(){
+    var sfxv = document.getElementById("SFXvolume").value;
+    SFXaudio.volume = sfxv/100;
+}
+
+function Mute(){
+    document.getElementById("BGvolume").value=0;
+    document.getElementById("SFXvolume").value=0;
+    SFXaudio.volume = 0.0;
+    BGaudio.volume = 0.0;
+    CBGaudio.volume = 0.0;
+    QBGaudio.volume = 0.0;
+}
+
+function Start(){
+    ToggleDis("StartBtn");
+    ToggleDis("ModeSelec");
+    SFXaudio.play();
+    document.getElementsByClassName("TxtDis")[0].innerHTML="Select a mode to start the game";
+}
+
+function Casual(){
+    document.getElementsByClassName("TxtDis")[0].innerHTML="Welcome to the Tic Tac Toe Game";
+    BGaudio.pause();
+    CBGaudio.play();
+    SFXaudio.play();
+    mode="casual";
+    ToggleDis("Stage1");
+    ToggleDis("Stage2");
+    document.body.style.backgroundColor= "var(--red-900)";
+    ToggleDis("Resume");
+    ToggleDis("MainMenu")
+    ToggleDis("Countdown");
+}
+
+function QuickDraw(){
+    document.getElementsByClassName("TxtDis")[0].innerHTML="Welcome to the Tic Tac Toe Game";
+    BGaudio.pause();
+    QBGaudio.play();
+    SFXaudio.play();
+    mode="quickDraw";
+    ToggleDis("Stage1");
+    ToggleDis("Stage2");
+    document.body.style.backgroundColor= "rgb(94, 4, 4)";
+    ToggleDis("Resume");
+    ToggleDis("MainMenu");
+    StartTimer(10,"Countdown");
+    loop =setInterval(()=>{
+        if(document.getElementsByClassName("Countdown")[0].innerHTML=="Time's up"){
+            let ply= turn%2==0?"player2":"player1";
+            EndGame(ply+"has won. due to Time out");
+            clearInterval(loop);
+        }
+    },1000);
+}
+
+function Back(){
+    SFXaudio.play();
+    ToggleDis("ModeSelec");
+    ToggleDis("StartBtn");
+    document.getElementsByClassName("TxtDis")[0].innerHTML="Welcome to the Tic Tac Toe Game";
+}
+
+function play(Id){
+    SFXaudio.pause();
+    SFXaudio.play();
+    var player1 = "X";
+    var player2 = "O";
+    if (turn%2==0&&board[Id]==0){
+        document.getElementById(Id).innerHTML=player1;
+        document.getElementById(Id).className="Player1";
+        board[Id]=1;
+        turn++;
+        document.getElementById("TurnDis").innerHTML="Player2's Turn";
+        document.getElementById("TurnDis").className="P2"
+        document.body.style.backgroundColor = "rgb(4, 28, 94)"
+    }
+    else if(turn%2!=0&&board[Id]==0){
+        document.getElementById(Id).innerHTML=player2;
+        document.getElementById(Id).className="Player2";
+        board[Id]=2;
+        turn++;
+        document.getElementById("TurnDis").innerHTML="Player1's Turn";
+        document.getElementById("TurnDis").className="P1"
+        document.body.style.backgroundColor = "rgb(94, 4, 4)";
+    }
+    StartTimer(10,"Countdown");
+    if (WinCondition(board)){
+        var message="Congratulations! Player"+winner+" wins.";
+        if(winner==1){
+            p1s++
+        }
+        else{
+            p2s++
+        }
+        EndGame(message);
+        UpdateScore();
+    }
+    else if (turn===9){
+        EndGame("Good play! It was a tie");
+    }
+}
+
+function Rematch(){
+    Reset();
+}
+
 function Quit(){
     SFXaudio.play();
     BGaudio.play();
     CBGaudio.pause();
     QBGaudio.pause();
-    reset();
-    toggleDis("stage1");
-    toggleDis("stage2");
+    Reset();
+    ToggleDis("Stage1");
+    ToggleDis("Stage2");
     p1s=0;
     p2s=0;
-    updateScore();
+    UpdateScore();
     document.body.style.backgroundColor= "rgb(255, 255, 255)";
-    toggleDis("modeSelec");
-    toggleDis("startbtn");
-    toggleDis("Resume");
-    toggleDis("MainMenu");
-    stopTimer();
+    ToggleDis("ModeSelec");
+    ToggleDis("StartBtn");
+    ToggleDis("Resume");
+    ToggleDis("MainMenu");
+    StopTimer();
     if(mode=="casual"){
-        toggleDis("countdown");
+        ToggleDis("Countdown");
     }
 }
-function Back(){
-    SFXaudio.play();
-    toggleDis("modeSelec");
-    toggleDis("startbtn");
-    document.getElementsByClassName("welcometxt")[0].innerHTML="Welcome to the Tic Tac Toe Game";
-}
-function timer(dur,obj_name){
+
+function StartTimer(dur,obj_name){
     if (timerID){
         clearInterval(timerID);
     }
     time = dur;
     var obj = document.getElementsByClassName(obj_name)[0]
     obj.innerHTML = time;
-    timerID = setInterval(() =>{
+    timerID = setInterval (() =>{
         if (time>0){
             time--;
             obj.innerHTML = time;
@@ -136,34 +252,13 @@ function timer(dur,obj_name){
     }
     ,1000)
 }
-function toggleMenu(){
-    SFXaudio.pause();
-    SFXaudio.play();
-    let menuObj=document.getElementsByClassName("menubtns")[0];
-    if (menuObj.style.display=="none"){
-        menuObj.style.display="flex";
-        document.getElementsByClassName("menuicon")[0].src="media/images/close.png"
-    }
-    else{
-        menuObj.style.display="none";
-        document.getElementsByClassName("menuicon")[0].src="media/images/menu.png"
-    }
-    if (timerID){
-        clearInterval(timerID);
-        timerID=null;
-    }
-    else{
-        timer(time,"countdown");
-    }
-    if (document.getElementsByClassName("Settings-page")[0].style.display=="flex"){
-        toggleDis("Settings-page");
-    }
-    if (document.getElementsByClassName("Htp-page")[0].style.display=="flex"){
-        toggleDis("Htp-page");
-    }
-    
+
+function StopTimer(){
+    clearInterval(timerID);
+    clearInterval(loop);
 }
-function toggleDis(obj_name){
+
+function ToggleDis(obj_name){
     var obj = document.getElementsByClassName(obj_name);
     if (obj.length > 0){
         for (i=0;i<obj.length;i++){
@@ -186,61 +281,23 @@ function toggleDis(obj_name){
     }
     console.log(obj);
 }
-function play(Id){
-    SFXaudio.pause();
-    SFXaudio.play();
-    var player1 = "X";
-    var player2 = "O";
-    if (turn%2==0&&board[Id]==0){
-        document.getElementById(Id).innerHTML=player1;
-        document.getElementById(Id).className="player1";
-        board[Id]=1;
-        turn++;
-        document.getElementById("pdis").innerHTML="Player2's Turn";
-        document.getElementById("pdis").className="p2"
-        document.body.style.backgroundColor = "rgb(4, 28, 94)"
-    }
-    else if(turn%2!=0&&board[Id]==0){
-        document.getElementById(Id).innerHTML=player2;
-        document.getElementById(Id).className="player2";
-        board[Id]=2;
-        turn++;
-        document.getElementById("pdis").innerHTML="Player1's Turn";
-        document.getElementById("pdis").className="p1"
-        document.body.style.backgroundColor = "rgb(94, 4, 4)";
-    }
-    timer(10,"countdown");
-    if (wincondition(board)){
-        var message="Congratulations! Player"+winner+" wins.";
-        if(winner==1){
-            p1s++
-        }
-        else{
-            p2s++
-        }
-        EndGame(message);
-        updateScore();
-    }
-    else if (turn===9){
-        EndGame("Good play! It was a tie");
-    }
-}
 
-function reset(){
+function Reset(){
     SFXaudio.play();
     turn=0;
     board=[0,0,0,0,0,0,0,0,0,0];
     for(i=1;i<10;i++){
         document.getElementById(i).innerHTML=null;
-        document.getElementById(i).classList.remove("player1","player2");
-        document.getElementById("pdis").innerHTML="Player1's Turn";
-        document.getElementById("pdis").className="p1"
+        document.getElementById(i).classList.remove("Player1","Player2");
+        document.getElementById("TurnDis").innerHTML="Player1's Turn";
+        document.getElementById("TurnDis").className="P1"
         document.body.style.backgroundColor = "rgb(94, 4, 4)";
     }
-    timer(10,"countdown");
-    toggleDis("stage3");
+    StartTimer(10,"Countdown");
+    ToggleDis("Stage3");
 }
-function wincondition(board){
+
+function WinCondition(board){
     if(0!=board[1]&&board[1]==board[4]&&board[1]==board[7]){
         winner=board[1];
         return true;
@@ -277,47 +334,29 @@ function wincondition(board){
         return false;
     }
 }
+
 function EndGame(message){
-    stopTimer();
-    toggleDis("stage3");
-    document.getElementsByClassName("endscr")[0].innerHTML=message;
-}
-function updateScore(){
-    document.getElementById("p1s").innerHTML="Player1's Score:"+p1s;
-    document.getElementById("p2s").innerHTML="Player1's Score:"+p2s;
-}
-function stopTimer(){
-    clearInterval(timerID);
-    clearInterval(loop);
-}
-function BGvolume(){
-    var bgv = document.getElementById("BGvolume").value;
-    BGaudio.volume = bgv/100;
-    CBGaudio.volume = bgv/100;
-    QBGaudio.volume = bgv/100;
-}
-function SFXvolume(){
-    var sfxv = document.getElementById("SFXvolume").value;
-    SFXaudio.volume = sfxv/100;
-}
-function Mute(){
-    document.getElementById("BGvolume").value=0;
-    document.getElementById("SFXvolume").value=0;
-    SFXaudio.volume = 0.0;
-    BGaudio.volume = 0.0;
-    CBGaudio.volume = 0.0;
-    QBGaudio.volume = 0.0;
+    StopTimer();
+    ToggleDis("Stage3");
+    document.getElementsByClassName("EndTxt")[0].innerHTML=message;
 }
 
+function UpdateScore(){
+    document.getElementById("P1S").innerHTML="Player1's Score:"+p1s;
+    document.getElementById("P2S").innerHTML="Player1's Score:"+p2s;
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    toggleDis("stage3");
-    toggleDis("stage2");
-    toggleDis("modeSelec");
-    toggleDis("menubtns");
-    toggleDis("Resume");
-    toggleDis("MainMenu");
-    toggleDis("Settings-page");
-    toggleDis("Htp-page");
+    //ToggleDis("Stage1");
+    ToggleDis("Stage2");
+    ToggleDis("Stage3");
+    ToggleDis("Resume");
+    ToggleDis("MainMenu");
+    ToggleDis("Settings-page");
+    ToggleDis("Htp-page");
+    ToggleDis("DropdownItems");
+    ToggleDis("ModeSelec");
 });
 document.addEventListener("load", () => {
     let BGaudio = document.getElementById("BGaudio");
